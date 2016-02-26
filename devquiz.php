7113b9ae4466e06d3ea3d1a1e3d21423c98bb38b
Imag1n3 <slavipetroff@gmail.com>
2016-02-27 01:23:56 +0200

    Changed: devquiz

--- devquiz.php
+++ devquiz.php
@@ -8,8 +8,8 @@ $token = filter_input(INPUT_POST, 'token', FILTER_SANITIZE_STRING);
 $command = filter_input(INPUT_POST, 'command', FILTER_SANITIZE_STRING);
 $text = filter_input(INPUT_POST, 'text', FILTER_SANITIZE_STRING);
 $channel_id = filter_input(INPUT_POST, 'channel_id', FILTER_SANITIZE_STRING);
-$user_name = filter_input(INPUT_POST, 'user_name', FILTER_SANITIZE_STRING);
-$user_id = filter_input(INPUT_POST, 'user_id', FILTER_SANITIZE_STRING);
+// $user_name = filter_input(INPUT_POST, 'user_name', FILTER_SANITIZE_STRING);
+// $user_id = filter_input(INPUT_POST, 'user_id', FILTER_SANITIZE_STRING);
 
 //get it from from slash command configuration in Slack.
 // if ($token !== "4f8uLdSjrKGQX4A8uXwsmZms") {
@@ -18,7 +18,7 @@ $user_id = filter_input(INPUT_POST, 'user_id', FILTER_SANITIZE_STRING);
 // }
 
 //create slack webhook and paste its url here.
-//$slack_webhook_url = "https://hooks.slack.com/services/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";
+$slack_webhook_url = "https://hooks.slack.com/services/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";
 
 //this is bojinkata's beautiful head.
 //$icon_url = "http://www4.pictures.zimbio.com/gi/AC+Chievo+Verona+v+Lecce+Serie+DuNWqy4FrZDm.jpg";
@@ -47,7 +47,7 @@ $user_id = filter_input(INPUT_POST, 'user_id', FILTER_SANITIZE_STRING);
 //     $posts = $result->liked_posts;
 //     $index = rand(0,(count($posts)-1));
 
-     $message_text = "<Здравей @".$user_id."|".$user_name."> призовавам те на Quiz дуел! Ако имаш играта цъкни тук (devquiz://play). Ако я нямаш, свали я от тук(https://www.dropbox.com/s/hsn6ow3b8lqt9u1/app-debug.apk?dl=0).\n";
+     $message_text = "<Здравей @".$text."> призовавам те на Quiz дуел! Ако имаш играта цъкни тук (devquiz://play). Ако я нямаш, свали я от тук(https://www.dropbox.com/s/hsn6ow3b8lqt9u1/app-debug.apk?dl=0).\n";
      //$message_text .= $posts[$index]->photos[0]->alt_sizes[0]->url;
 
         //TO HERE: is the Tumblr stuff. You can change it with whatever your slash command should do.
